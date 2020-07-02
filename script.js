be4466afe28fd9cd062e93140a98f47252e727e7
Roberto Riccioli <riccioli.rob@gmail.com>
2020-07-02 17:13:23 +0200

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".header-right i.fa-bars").click(
+  function () {
+    $(".hamburger-menu").show();
+  }
+);
+
+$(".hamburger-menu i.fa-times").click(
+  function (){
+    $(".hamburger-menu").hide();
+  }
+);
